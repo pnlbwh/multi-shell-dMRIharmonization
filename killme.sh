ab9924fca3748a2fe0cf28260b05f230c86ea1ee
EC2 Default User <ec2-user@ip-172-31-17-45.ec2.internal>
2023-06-05 19:08:39 +0000

    updated readme

---
+++ killme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+kill 29316
+kill 29324
+kill 29332
+kill 29340
+kill 29348
+kill 29356
+kill 29364
+kill 29372
+kill 29380
+kill 29388
+kill 29396
+kill 29404
+kill 29412
+kill 29420
+kill 29428
+kill 29436
