f0039e4f4cd6475087a43ace72823f021e16abae
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-06-23 12:49:26 +0000

    fixing errors caused when rebasing

--- lib/consistencyCheck.py
+++ lib/consistencyCheck.py
@@ -23,46 +23,20 @@ import sys
 
 
 def check_bshells(ref_imgs, ref_bvals):
-    unmatched = []
+
+    unmatched=[]
     for imgPath in ref_imgs:
 
-        imgPath = local.path(imgPath)
+        imgPath= local.path(imgPath)
         if not imgPath.exists():
             FileNotFoundError(imgPath)
 
         inPrefix = abspath(imgPath).split('.nii')[0]
-        bvals = findBShells(inPrefix + '.bval')
+        bvals= findBShells(inPrefix+'.bval')
 
-        # if (bvals == ref_bvals).all():
-        #     print("b-shells matched for", imgPath.name)
-        # do the above check so the following error does not happen: AttributeError: 'bool' object has no attribute 'all'
-        # AttributeError: 'bool' object has no attribute 'all' by checking if it is single values or array of values
-        print("b-shells matched for", imgPath.name)
-        # convert to numpy array first
-        if not isinstance(bvals, np.ndarray):
-            bvals = np.array(bvals)
-        if not isinstance(ref_bvals, np.ndarray):
-            ref_bvals = np.array(ref_bvals)
-        # print out the type of bvals and ref_bvals
-        print("bvals: ", type(bvals))
-        print("ref_bvals: ", type(ref_bvals))
-        # print out the shape of bvals and ref_bvals
-        print("bvals.shape: ", bvals.shape)
-        print("ref_bvals.shape: ", ref_bvals.shape)
-        # print out the values of bvals and ref_bvals
-        print("bvals: ", bvals)
-
-        print("ref_bvals: ", ref_bvals)
-        # print the image path for debugging
-        print("************ imgPath: ", imgPath)
-    
-        if (bvals == ref_bvals).all():
-<<<<<<< HEAD
-            print("b-shells matched for", imgPath.name)
-=======
+        if (bvals==ref_bvals).all():
             print('b-shells matched for', imgPath.name)
 
->>>>>>> 8568ed5 (reverting consistencyCheck back to original after debugging)
         else:
             print(f'\nUnmatched b-shells for {imgPath.name}')
             print(bvals)
@@ -73,25 +47,26 @@ def check_bshells(ref_imgs, ref_bvals):
     if len(unmatched):
         print('Unmatched cases:')
         print(unmatched)
-        raise ValueError(
-            'Leave out the unmatched cases or change the reference case for determining b-shell to run multi-shell-dMRIharmonization')
+        raise ValueError('Leave out the unmatched cases or change the reference case for determining b-shell to run multi-shell-dMRIharmonization')
 
     else:
         print('All cases have same b-shells. Data is good for running multi-shell-dMRIharmonization')
     print('')
 
 
+
 def check_resolution(ref_imgs, ref_res):
+
     unmatched = []
     for imgPath in ref_imgs:
 
-        imgPath = local.path(imgPath)
+        imgPath= local.path(imgPath)
         if not imgPath.exists():
             FileNotFoundError(imgPath)
 
-        res = load(imgPath._path).header['pixdim'][1:4]
+        res= load(imgPath._path).header['pixdim'][1:4]
 
-        if (res - ref_res).sum() <= 10e-6:
+        if (res-ref_res).sum()<=10e-6:
             print('spatial resolution matched for', imgPath.name)
 
         else:
@@ -104,22 +79,23 @@ def check_resolution(ref_imgs, ref_res):
     if len(unmatched):
         print('Unmatched cases:')
         print(unmatched)
-        raise ValueError(
-            'Leave out the unmatched cases or change the reference case for determining spatial resolution to run multi-shell-dMRIharmonization')
+        raise ValueError('Leave out the unmatched cases or change the reference case for determining spatial resolution to run multi-shell-dMRIharmonization')
 
     else:
         print('All cases have same spatial resolution. Data is good for running multi-shell-dMRIharmonization')
     print('')
 
 
-def consistencyCheck(ref_csv, outputBshellFile=None, outPutResolutionFile=None):
+def consistencyCheck(ref_csv, outputBshellFile= None, outPutResolutionFile= None):
+
     try:
         ref_imgs, _ = read_imgs_masks(ref_csv)
     except:
         ref_imgs = read_imgs(ref_csv)
 
+
     if isfile(outputBshellFile) and isfile(outPutResolutionFile):
-        ref_bvals = read_bvals(outputBshellFile)
+        ref_bvals= read_bvals(outputBshellFile)
         ref_res = np.load(outPutResolutionFile)
     else:
         ref_bshell_img = ref_imgs[0]
@@ -131,6 +107,7 @@ def consistencyCheck(ref_csv, outputBshellFile=None, outPutResolutionFile=None):
         ref_res = load(ref_bshell_img).header['pixdim'][1:4]
         np.save(outPutResolutionFile, ref_res)
 
+
     print('b-shells are', ref_bvals)
 
     print('\nSite', ref_csv, '\n')
@@ -138,13 +115,15 @@ def consistencyCheck(ref_csv, outputBshellFile=None, outPutResolutionFile=None):
     print('Checking consistency of b-shells among subjects')
     check_bshells(ref_imgs, ref_bvals)
 
+
     print('spatial resolution is', ref_res)
     print('Checking consistency of spatial resolution among subjects')
     check_resolution(ref_imgs, ref_res)
 
 
+
 if __name__ == '__main__':
-    if len(sys.argv) == 1 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
+    if len(sys.argv)==1 or sys.argv[1]=='-h' or sys.argv[1]=='--help':
         print('''Check consistency of b-shells and spatial resolution among subjects
 Usage:
 consistencyCheck list.csv/txt ref_bshell_bvalues.txt ref_res_file.npy
@@ -154,9 +133,9 @@ or just one column for dwi1\\ndwi2\\n...
 In addition, provide ref_bshell_bvalues and ref_res_file.''')
         exit()
 
-    ref_csv = abspath(sys.argv[1])
-    outputBshellFile = abspath(sys.argv[2])
-    outPutResolutionFile = abspath(sys.argv[3])
+    ref_csv= abspath(sys.argv[1])
+    outputBshellFile= abspath(sys.argv[2])
+    outPutResolutionFile= abspath(sys.argv[3])
     if isfile(ref_csv):
         consistencyCheck(ref_csv, outputBshellFile, outPutResolutionFile)
     else:
